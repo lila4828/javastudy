c2a7e324f1388ed4ebaab50ee7c14d51110b7f2a
7312-007 <djdj48285@naver.com>
2023-10-04 14:45:06 +0900

    5,6장 실습

---
+++ src/main/java/com/example/demo/data/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.data.repository;
+
+import com.example.demo.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 예제 6.7
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
