c067e2d69abe4a3914a10f34a2d0865ee1274ecc
7312-007 <djdj48285@naver.com>
2023-11-13 17:28:49 +0900

    11-13 수업

---
+++ src/main/java/com/example/java1113/Java1113Application.java
@@ -0,0 +1,13 @@
+package com.example.java1113;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java1113Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java1113Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java1113/Java1113ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java1113;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java1113ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
